62f1317136547a28ff687c76b54450b24d1f4b1b
wmmxk <wmmkx@hotmail.com>
2017-10-10 11:48:13 -0700

    test a function

---
+++ adder/target/debug/adder-18e097b064df4df5.d
@@ -0,0 +1,1 @@
+/home/wxk/Git/OOP_rust/adder/target/debug/adder-18e097b064df4df5: /home/wxk/Git/OOP_rust/adder/src/lib.rs
---
+++ adder/target/debug/libadder.d
@@ -0,0 +1,1 @@
+/home/wxk/Git/OOP_rust/adder/target/debug/libadder.rlib: /home/wxk/Git/OOP_rust/adder/src/lib.rs
